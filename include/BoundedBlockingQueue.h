60eee550421fa93e289c0c66df53e13a60e0a2d3
wonglyxng <wspoys@gmail.com>
2020-12-20 23:04:35 +0800

    add bounded

--- include/BoundedBlockingQueue.h
+++ include/BoundedBlockingQueue.h
@@ -13,20 +13,25 @@ template<typename T>
 class BoundedBlockingQueue {
 
 public:
-    explicit BoundedBlockingQueue(int capacity);
+    explicit BoundedBlockingQueue(size_t max_size);
+    BoundedBlockingQueue(const BoundedBlockingQueue&) = delete;
+    BoundedBlockingQueue& operator = (const BoundedBlockingQueue&) = delete;
 
     virtual ~BoundedBlockingQueue();
 
     void enqueue(const T& element);
     void dequeue(T& element);
-    int size();
-    bool empty();
+    size_t size() const;
+    size_t capacity() const;
+    bool empty() const;
+    bool full() const;
 
 private:
     std::queue<T> q_;
-    int cap_{};
+    size_t cap_{};
     mutable std::mutex m_;
-    std::condition_variable cv_;
+    std::condition_variable cv_not_full_;
+    std::condition_variable cv_not_empty_;
 };
 
 
--- src/BoundedBlockingQueue.cc
+++ src/BoundedBlockingQueue.cc
@@ -5,9 +5,8 @@
 #include "../include/BoundedBlockingQueue.h"
 
 template<typename T>
-BoundedBlockingQueue<T>::BoundedBlockingQueue(int capacity)
+BoundedBlockingQueue<T>::BoundedBlockingQueue(size_t capacity)
         : cap_(capacity) {
-
 }
 
 template<typename T>
@@ -17,30 +16,42 @@ template<typename T>
 void BoundedBlockingQueue<T>::enqueue(const T& element) {
     std::unique_lock<std::mutex> l(m_);
     while (q_.size() >= cap_){
-        cv_.wait(l);
+        cv_not_full_.wait(l);
     }
     q_.push(element);
-    cv_.notify_one();
+    cv_not_empty_.notify_one();
 }
 
 template<typename T>
 void BoundedBlockingQueue<T>::dequeue(T& element) {
     std::unique_lock<std::mutex> l(m_);
     while (q_.empty()){
-        cv_.wait(l);
+        cv_not_empty_.wait(l);
     }
     element = q_.front();
     q_.pop();
+    cv_not_full_.notify_one();
 }
 
 template<typename T>
-int BoundedBlockingQueue<T>::size() {
+size_t BoundedBlockingQueue<T>::size() const {
     std::unique_lock<std::mutex> l(m_);
     return q_.size();
 }
 
 template<typename T>
-bool BoundedBlockingQueue<T>::empty() {
+bool BoundedBlockingQueue<T>::empty() const {
     std::unique_lock<std::mutex> l(m_);
     return q_.empty();
+}
+
+template<typename T>
+size_t BoundedBlockingQueue<T>::capacity() const {
+    return cap_;
+}
+
+template<typename T>
+bool BoundedBlockingQueue<T>::full() const {
+    std::unique_lock<std::mutex> l(m_);
+    return q_.size() >= cap_;
 }
